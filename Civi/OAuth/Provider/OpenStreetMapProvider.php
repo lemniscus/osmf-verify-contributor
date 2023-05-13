04dba9c1fac7157ceaedd7d808bd9208b4e08094
Noah Miller <nm@lemnisc.us>
2023-05-13 11:59:27 -0700

    OAuth provider: new way of enforcing public return url

--- Civi/OAuth/Provider/OpenStreetMapProvider.php
+++ Civi/OAuth/Provider/OpenStreetMapProvider.php
@@ -8,7 +8,13 @@ use League\OAuth2\Client\Token\AccessToken;
 
 class OpenStreetMapProvider extends \Civi\OAuth\CiviGenericProvider {
 
+  public function __construct(array $options = [], array $collaborators = []) {
+    $options['redirectUri'] = \CRM_Utils_System::url('civicrm/oauth-client/return', NULL, TRUE, NULL, FALSE, TRUE, FALSE);
+    parent::__construct($options, $collaborators);
+  }
+
   protected function createResourceOwner(array $response, AccessToken $token): OpenStreetMapUser {
     return new OpenStreetMapUser($response);
   }
+
 }
